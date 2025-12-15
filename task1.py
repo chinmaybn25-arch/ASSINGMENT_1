9bc83e7dd9e63dbd904466b4272db8ecf695a683
chinmaybn25-arch <chinmaybn25@gmail.com>
2025-12-15 20:47:43 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,15 @@
+x= int(input('enter the first number:'))
+y= int(input('enter the second number:'))
+z= x+y
+a = x-y
+b = x*y
+c=x/y
+print("addition is:",z)
+print('subtraction is:',a)
+print("multiplication is:",b)
+print("division is:",c)
+
+
+
+
+
---
+++ task2.py
@@ -0,0 +1,3 @@
+x= input('enter your first name:')
+y= input('enter your last name:')
+print(f"{x} {y}! welcome to the 'PYTHON PARTY'.",)
